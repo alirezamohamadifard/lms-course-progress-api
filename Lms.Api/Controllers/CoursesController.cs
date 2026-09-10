6793aaf3c0ed09afdeb81fab2059a5bbb54b7fea
Alireza <alireza.mohamadifar4045@gamil.com>
2026-09-10 18:58:01 +0330

    feat: add course API endpoints

--- Lms.Api/Controllers/CoursesController.cs
+++ Lms.Api/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Lms.Application.Contracts.Courses;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lms.Api.Controllers
@@ -7,5 +9,33 @@ namespace Lms.Api.Controllers
     [ApiController]
     public class CoursesController : ControllerBase
     {
+        private readonly ICourseService _courseService;
+        public CoursesController(ICourseService corsService)
+        {
+            _courseService = corsService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CourseDto>> CreateAsync(CreateCourseRequest request, CancellationToken cancellationToken)
+        {
+            var Course = await  _courseService.CreateAsync(request, cancellationToken);
+            return CreatedAtAction("GetById", new { id = Course.Id }, Course);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyCollection<CourseDto>>> GetAll(CancellationToken cancellationToken)
+        { 
+            var courses = await _courseService.GetAllAsync(cancellationToken);
+            return Ok(courses);
+        }
+
+        [HttpGet("id:guid")]
+        public async Task<ActionResult<CourseDto>> GetById (Guid id, CancellationToken cancellationToken)
+        {
+            var course = await _courseService.GetByIdAsync(id, cancellationToken);
+            return course is null
+            ? NotFound()
+            : Ok(course);
+        }
     }
 }
--- Lms.Api/Program.cs
+++ Lms.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
@@ -17,6 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
 
