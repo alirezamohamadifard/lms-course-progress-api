cf5afd38598fd38f7bd03d8d62c17273ef3eefbd
Alireza <alireza.mohamadifar4045@gamil.com>
2026-09-10 20:19:26 +0330

    feat: add lesson API endpoints

---
+++ Lms.Api/Controllers/LessonsController.cs
@@ -0,0 +1,47 @@
+﻿using Lms.Application.Contracts.Lessons;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Lms.Api.Controllers
+{
+    [Route("api/courses/{courseId:guid}/lessons")]
+    [ApiController]
+    public class LessonsController : ControllerBase
+    {
+        private readonly ILessonService _lessonService;
+
+        public LessonsController(ILessonService lessonService)
+        {
+            _lessonService = lessonService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<LessonDto>> Create(Guid courseId, CreateLessonRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var lesson = await _lessonService.CreateAsync(courseId, request, cancellationToken);
+
+                return CreatedAtAction(nameof(GetByCourseId), new { courseId }, lesson);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound(new
+                {
+                    message = "Course was not found."
+                });
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<LessonDto>>> GetByCourseId(Guid courseId, CancellationToken cancellationToken)
+        {
+            var lessons = await _lessonService.GetByCourseIdAsync(
+            courseId,
+            cancellationToken);
+
+            return Ok(lessons);
+        }
+    }
+}
