6c2576daf9af97f160ec0029727ad63c952474bc
Alireza <alireza.mohamadifar4045@gamil.com>
2026-09-11 19:17:07 +0330

    feat: add authentication API endpoints

---
+++ Lms.Api/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Lms.Application.Contracts.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lms.Api.Controllers
+{
+    [Route("api/Auth")]
+    [ApiController]
+    [AllowAnonymous]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponse>> Rigister(RegisterRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _authService.RegisterAsync(request, cancellationToken);
+            return StatusCode(StatusCodes.Status201Created, response);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(LoginRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _authService.LoginAsync(request, cancellationToken);
+
+            return Ok(response);
+        }
+    }
+}
