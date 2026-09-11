a4ff5d322d676ce40cad6ed539bd87ffc998e9d4
Alireza <alireza.mohamadifar4045@gamil.com>
2026-09-11 18:35:25 +0330

    feat: add password hashing and JWT token generation

---
+++ Lms.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,56 @@
+﻿using Lms.Domain.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Lms.Infrastructure.Authentication
+{
+    public sealed class JwtTokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Generator (User user)
+        {
+            var Issuer = _configuration["Jwt:Issuer"] 
+                ?? throw new InvalidOperationException("jwt issuer not configured");
+
+            var Audience = _configuration["Jwt:Audience"]
+                ?? throw new InvalidOperationException("JWT audience was not configured.");
+
+            var key = _configuration["Jwt:Key"]
+                ?? throw new InvalidOperationException("JWT Key Was not configured");
+
+            var expiryInMinutes = _configuration.GetValue<int>("Jwt:ExpiryInMinutes");
+
+            var claims = new List<Claim>
+            {
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.Email, user.Email),
+                new(ClaimTypes.Name, user.FullName),
+                new(ClaimTypes.Role, user.Role.ToString()),
+                new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            
+            var credentials = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: Issuer,
+                audience: Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Lms.Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lms.Infrastructure.Authentication
+{
+    public sealed class PasswordHasher
+    {
+        private const int SaltSize = 16;
+        private const int HashSize = 32;
+        private const int Itrations = 600_000;
+
+        public string Hash(string password)
+        {
+            var salt = RandomNumberGenerator.GetBytes(SaltSize);
+
+            var hash = Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                Itrations,
+                HashAlgorithmName.SHA256,
+                HashSize);
+
+            return string.Join(
+                ".",
+                Itrations,
+                Convert.ToBase64String(salt),
+                Convert.ToBase64String(hash));
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            var parts = passwordHash.Split('.');
+
+            if (parts.Length != 3 || !int.TryParse(parts[0], out var iterations))
+                return false;
+
+            var salt = Convert.FromBase64String(parts[1]);
+            var expectedhash = Convert.FromBase64String(parts[2]);
+
+            var actualhash = Rfc2898DeriveBytes.Pbkdf2(
+                password,
+                salt,
+                Itrations,
+                HashAlgorithmName.SHA256,
+                HashSize);
+
+            return CryptographicOperations.FixedTimeEquals(actualhash, expectedhash);
+        }
+    }
+}
